6cbe9d08558ac44a587d05bdac2e97c9555148de
phngkhuongduy <phngkhuongduy@gmail.com>
2021-10-05 17:18:41 +0700

    change from sqlx to sql

--- connector/connector.go
+++ connector/connector.go
@@ -1,8 +1,8 @@
 package connector
 
 import (
+	"database/sql"
 	_ "github.com/ClickHouse/clickhouse-go"
-	"github.com/jmoiron/sqlx"
 	"log"
 )
 
@@ -11,16 +11,11 @@ const (
 	password = ",X7h(_JT){bKcL$k"
 	database = "liquidity_pool"
 	)
-func ConnectToClickHouse() (connect *sqlx.DB, err error){
-	connect, err = sqlx.Open("clickhouse", "tcp://127.0.0.1:9000?username="+username+"&password=" + password +"&database="+database+"&debug=true")
+func ConnectToClickHouse() (connect *sql.DB, err error){
+	connect, err = sql.Open("clickhouse", "tcp://127.0.0.1:9000?username="+username+"&password=" + password +"&database="+database+"&debug=true")
 	if err != nil {
 		log.Fatal(err)
 		return nil, err
 	}
-	err = connect.Ping()
-	if err != nil {
-		log.Println(err)
-	}
-
 	return connect, nil
 }
